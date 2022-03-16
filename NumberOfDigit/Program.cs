7647d7de430afece7166f71b897979cb6d4fe12b
Ваше имя <garaevaea@mail.ru>
2022-03-16 18:58:28 +0300

    Add NumberOfDigit

---
+++ NumberOfDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumberOfDigit(int number)
+{
+    int numberOfDigits = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        numberOfDigits++;
+        Console.WriteLine(number);
+    }
+    return numberOfDigits;
+}
+
+while(true)
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int numberOfDigits = GetNumberOfDigit(number);
+    Console.WriteLine($"количество цифр {numberOfDigits}");
+}
