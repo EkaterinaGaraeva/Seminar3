763a7cfdac0c6d0e5701d21b0182e59668db2fe9
Ваше имя <garaevaea@mail.ru>
2022-03-16 03:39:08 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int quarterNumber = 1;
+
+switch (quarterNumber)
+{
+    case 1:
+    {
+        Console.WriteLine("X > 0, Y > 0");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("X < 0, Y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("X < 0, Y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("X > 0, Y < 0");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Не является номером четверти");
+        break;
+    }
+}
