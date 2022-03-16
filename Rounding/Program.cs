5cb382f8be7cfd4ef9c6f4022d88a12fc2a3e97f
Ваше имя <garaevaea@mail.ru>
2022-03-16 19:08:55 +0300

    Add Rounding

---
+++ Rounding/Program.cs
@@ -0,0 +1,8 @@
+﻿double someInfoData = 5.992547782;
+
+int data = (int)someInfoData;
+double data2 = Math.Round(someInfoData,2);
+
+Console.WriteLine(someInfoData);
+Console.WriteLine(data);
+Console.WriteLine(data2);
